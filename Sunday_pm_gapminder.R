7c998e6c599af449cca69ef2295bb22ee4664e85
TouyiaVue <touyiavue@gmail.com>
2015-04-19 15:00:36 -0500

    install dplyr package

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -4,7 +4,7 @@
 # April 19th, 2015
 
 # install dplyr package and dependencies
-install.packages("dplyr", dependencies = TRUE)
+# install.packages("dplyr", dependencies = TRUE)
 
 # Read in data as table-delimited
 gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t", header = TRUE)
@@ -13,5 +13,19 @@ gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t", header = TRUE)
 gap.in[1:15,1:6]
 
 add.me(3,4)
+
+gap.in %>% filter(pop > 15000000) %>% select(country, year, pop, lifeExp) 
+
+x <- gap.in %>% 
+  filter(year < 1990) %>% 
+  group_by(continent, year) %>% 
+  summarize (min = mean(pop))
+
+attach(iris)
+
+# install.packages("tidyr", dependencies = TRUE)
+
+# load tidyr package
+library("tidyr")
 
 
