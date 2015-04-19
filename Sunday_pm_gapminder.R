f05af0a97a0e18331a7a2ccd600f5a6c899c87ac
TouyiaVue <touyiavue@gmail.com>
2015-04-19 14:00:24 -0500

    add.me function

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -4,4 +4,9 @@
 # April 19th, 2015
 
 # Read in data as table-delimited
-gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t") 
+gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t", header = TRUE)
+
+
+gap.in[1:15,1:6]
+
+add.me(3,4)
